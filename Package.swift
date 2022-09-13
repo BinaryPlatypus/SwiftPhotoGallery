add2e0cd12034a266fe8c11f9ace22cc54edaabb
Richard Hodge <richard.hodge@flueid.com>
2022-09-13 13:06:38 -0500

    Updated to swift 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftPhotoGallery",
     platforms: [
-        .iOS(.v13), .tvOS(.v13)
+        .iOS(.v14), .tvOS(.v14)
     ],
     products: [
         .library(
